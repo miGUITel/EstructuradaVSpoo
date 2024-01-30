f1095a9989fd9b40f9cdb57b16b4ef97f12abe29
miGUITel <miguel.trigueros@aulaxxi.murciaeduca.es>
2024-01-30 17:22:17 +0100

    Versión inicial

---
+++ src/TransferenciaBancariaEstructurada.java
@@ -0,0 +1,22 @@
+public class TransferenciaBancariaEstructurada {
+    public static void main(String[] args) {
+        // Definir datos de cuentas
+        int cuentaOrigen = 123456;
+        int cuentaDestino = 789012;
+        double saldoOrigen = 500.0;
+        double saldoDestino = 200.0;
+
+        // Imprimir saldos antes de la transferencia
+        System.out.println("Saldo de la cuenta origen nº: " + cuentaOrigen + " = " + saldoOrigen);
+        System.out.println("Saldo de la cuenta destino nº: " + cuentaDestino + " = " + saldoDestino);
+
+        // Realizar la transferencia
+        System.out.println("Transferencia exitosa de 100 €");
+        saldoOrigen -= 100.0;
+        saldoDestino += 100.0;
+
+        // Imprimir saldos después de la transferencia
+        System.out.println("Saldo de la cuenta origen nº: " + cuentaOrigen + " = " + saldoOrigen);
+        System.out.println("Saldo de la cuenta destino nº: " + cuentaDestino + " = " + saldoDestino);
+    }
+}
