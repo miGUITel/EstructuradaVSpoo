9a7b855d1b65b5b2fe9ec8d0362196661fea7904
Miguel <migueltxiu@gmail.com>
2024-02-12 12:18:56 +0100

    Añado la clase Puerta a Instituto para mostrar el funcionamiento de los constructores Cuando se escribe el primer constructor de una clase se elimna el constructor por defecto

---
+++ src/Instituto/Casa.java
@@ -0,0 +1,13 @@
+package Instituto;
+
+public class Casa {
+    public static void main(String[] args) {
+
+        Puerta mipuerta = new Puerta();
+
+        mipuerta.abrir();
+
+    }
+
+
+}
---
+++ src/Instituto/Puerta.java
@@ -0,0 +1,16 @@
+package Instituto;
+
+public class Puerta {
+
+    public Puerta(int anyos){
+        this.edad = anyos;
+    }
+
+
+
+    //Atributos
+    private int edad;
+
+    //Métodos
+
+}
