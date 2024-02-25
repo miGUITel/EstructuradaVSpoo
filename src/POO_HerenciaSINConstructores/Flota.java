87b68fb570ba8376071d5fc4671262b5cbf6086f
miguel13 <migueltmz@outlook.es>
2024-02-25 11:53:49 +0100

    Modifico la clase Flota para que ayude a mostrar los métodos que están disponibles según la posición que ocupe la clase en la jerarquía de herencia.

--- src/POO_HerenciaSINConstructores/Flota.java
+++ src/POO_HerenciaSINConstructores/Flota.java
@@ -1,9 +1,14 @@
 package POO_HerenciaSINConstructores;
 
 public class Flota {
-
+//Cambiando la clase del siguiente atributo se puede ver cómo
+//    tiene acceso o no a los métodos de la jerarquía de herencia
+//    cuanto más abajo esté en la jerarquía más métodos (publicos)
+//    puede utilizar
+//    probar a cambiar por public Acuatico y public Vehiculo
     public Barco miYate = new Barco();
-
+//    public Vehiculo miYate = new Vehiculo();
+//    public Acuatico miYate = new Acuatico();
     void probar(){
         miYate.navegar();
         miYate.encenderMotor();
