b7065b206eab34c05bc38086a82588530f5a1c58
miguel13 <migueltmz@outlook.es>
2025-01-08 18:56:20 +0100

    Creo la clase aula para mostrar que puede pasar un mensaje.

---
+++ src/Calculadora/Aula.java
@@ -0,0 +1,13 @@
+package Calculadora;
+
+public class Aula {
+
+    public static void main(String[] args) {
+
+        //En el aula hay un alumno que puede hacer un Examen. Para ello, hace uso de su calculadora.
+
+        Alumno bernardo = new Alumno();
+        bernardo.hacerExamen();
+    }
+
+}
